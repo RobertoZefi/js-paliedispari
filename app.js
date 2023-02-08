c73f75adf9fab1a45455b82a8c00ce7477837210
RobertoZefi <zefi.roberto2001@gmail.com>
2023-02-08 18:31:29 +0100

    FEAT esercizio Palidroma finito

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 console.log ('daje')
 
-// chiedere un nome all'utente
 let nomeUtente = prompt('Inserisci un nome')
-// prendere il nome dell'utente e dividerlo lettera per lettera 
-let nomeLettere = nomeUtente.split('')
-console.log(nomeLettere, nomeLettere.length)
-// creare una funzione che va a ciclare le lettere del nome
 
-// stampare risulato   
+parolaPalindromeFunction(nomeUtente)
 
-
-
-for (let i = 0; i < nomeLettere.length/2; i++){
-    let lettera = nomeLettere[i]
-    let ultimaLettera = nomeLettere.length - 1
-    console.log (lettera, nomeLettere[ultimaLettera - i])
-    if (lettera != nomeLettere[ultimaLettera - i]){
-        //console.log (lettera, nomeLettere)
-        console.log ('no')
-    } else {
-        console.log('si')
+function parolaPalindromeFunction (nome) {
+    let parolaPalindrome = true
+    let nomeLettere = nomeUtente.split('')
+    
+    for (let i = 0; i < nomeLettere.length/2; i++){
+        let lettera = nomeLettere[i]
+        let ultimaLettera = nomeLettere.length - 1
+        //console.log (lettera, nomeLettere[ultimaLettera - i])
+        if (lettera != nomeLettere[ultimaLettera - i]){
+            //console.log (lettera, nomeLettere)
+            parolaPalindrome = false
+        }
+    }
+    
+    if (parolaPalindrome === true){
+        console.log ('La parola',nomeUtente,'è palindrome')
+    } else{
+        console.log ('La parola',nomeUtente,'non è palindrome')
     }
 }
 
-//nomeLettere[ultimaLettera - i]
+
+
+
